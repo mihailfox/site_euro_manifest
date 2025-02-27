1651f8cf0318efe1bf7349d9ebbfeef52b469397
Mihai Fratila <mihai.fratila01@gmail.com>
2025-02-27 22:20:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const faqItem = button.closest('.faq-item');
+      const answer = faqItem.querySelector('.faq-answer');
+      const icon = button.querySelector('svg');
+  
+      // Toggle hidden
+      answer.classList.toggle('hidden');
+  
+      // Rotește/transformă icon (ex. plus -> minus)
+      icon.classList.toggle('rotate-45');
+    });
+  });
+
+  document.querySelectorAll(".nav-menu li a").forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const sectionId = this.getAttribute("href").replace("#", "");
+      scrollToSection(sectionId);
+    });
+  });
+
+  function scrollToSection(sectionId) {
+    document
+      .getElementById(sectionId)
+      .scrollIntoView({ behavior: "smooth", block: "center" });
+  }
